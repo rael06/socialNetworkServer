858898f8bb5b681b18dd92237526b22ff7025595
rael <rael06@hotmail.fr>
2019-08-24 09:45:45 +0200

    Server done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,7 @@ import java.net.Socket;
 public class Main {
 
     public static void main(String[] args) {
+
         try (ServerSocket serverSocket = new ServerSocket(Constants.PORT)) {
             do {
                 Socket socket = serverSocket.accept();
